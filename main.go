7f471efc76211358c4a29b6178ddb874b868e304
Matheus Azzi <mazzic.dev@gmail.com>
2025-02-13 14:48:58 -0300

    websocket gorilla package removed then imported and not used

--- main.go
+++ main.go
@@ -3,25 +3,17 @@ package main
 import (
 	"context"
 	"log"
-	"net/http"
 	"time"
 
 	"learning-go/handlers"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var client *mongo.Client
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true // Permite todas as origens (não use isso em produção)
-	},
-}
 
 func init() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -60,7 +52,6 @@ func main() {
 	}))
 
 	// Rotas CRUD
-	r.GET("/ws", handleWebSocket)
 	r.POST("/users", handlers.CreateUser)
 	r.GET("/users", handlers.GetUsers)
 	r.PUT("/users/:email", handlers.UpdateUser)
@@ -74,28 +65,4 @@ func main() {
 
 	// Inicia o servidor na porta 8080
 	r.Run(":8080")
-}
-
-func handleWebSocket(c *gin.Context) {
-	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Falha ao abrir conexão WebSocket"})
-		return
-	}
-	defer conn.Close()
-
-	for {
-		// Lê uma mensagem do cliente
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("Erro ao ler mensagem:", err)
-			break
-		}
-
-		// Envia uma resposta ao cliente
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println("Erro ao enviar mensagem:", err)
-			break
-		}
-	}
 }
